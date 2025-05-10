e5cb326de6f15509e0416a97ced7f494257f3676
minjoowon <adom6789@naver.com>
2025-05-10 22:06:14 +0900

    aa

--- Minseo/pendulum/pendulum/Form1.Designer.cs
+++ Minseo/pendulum/pendulum/Form1.Designer.cs
@@ -29,12 +29,97 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timeStop = new System.Windows.Forms.Button();
+            this.timeReStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox2.Image = global::pendulum.Properties.Resources.New_Piskel__2_;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 259);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(788, 179);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::pendulum.Properties.Resources._1;
+            this.pictureBox1.Location = new System.Drawing.Point(207, 322);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(92, 89);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(446, 30);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(296, 88);
+            this.listBox1.TabIndex = 3;
+            // 
+            // timeStop
+            // 
+            this.timeStop.Location = new System.Drawing.Point(12, 21);
+            this.timeStop.Name = "timeStop";
+            this.timeStop.Size = new System.Drawing.Size(75, 23);
+            this.timeStop.TabIndex = 4;
+            this.timeStop.Text = "일시정지";
+            this.timeStop.UseVisualStyleBackColor = true;
+            this.timeStop.Click += new System.EventHandler(this.timeStop_Click);
+            // 
+            // timeReStart
+            // 
+            this.timeReStart.Location = new System.Drawing.Point(12, 50);
+            this.timeReStart.Name = "timeReStart";
+            this.timeReStart.Size = new System.Drawing.Size(75, 23);
+            this.timeReStart.TabIndex = 5;
+            this.timeReStart.Text = "재시작";
+            this.timeReStart.UseVisualStyleBackColor = true;
+            this.timeReStart.Click += new System.EventHandler(this.timeReStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.timeReStart);
+            this.Controls.Add(this.timeStop);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button timeStop;
+        private System.Windows.Forms.Button timeReStart;
     }
 }
 
--- Minseo/pendulum/pendulum/Form1.cs
+++ Minseo/pendulum/pendulum/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,94 @@ namespace pendulum
         public Form1()
         {
             InitializeComponent();
+
+
+
+
+
+            x_pos = pictureBox1.Location.X;
+            pictureBox2.SendToBack();
+            timer1.Start();
+        }
+
+        public Bitmap ChangeOpacity(Image img, float opacityvalue)
+        {
+            Bitmap bmp = new Bitmap(img.Width, img.Height);
+            Graphics graphics = Graphics.FromImage(bmp);
+            ColorMatrix colormatrix = new ColorMatrix();
+            colormatrix.Matrix33 = opacityvalue;
+            ImageAttributes imgAttribute = new ImageAttributes();
+            imgAttribute.SetColorMatrix(colormatrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+            graphics.DrawImage(img, new Rectangle(0, 0, bmp.Width, bmp.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, imgAttribute);
+            graphics.Dispose();
+            return bmp;
+        }
+
+
+        private static float num = 0;
+        private static float speed = 0;
+        private const float gravity = -0.5f;
+        private static float v_pos = 0;
+        private static int x_pos;
+        private static float t = 0.3f;
+
+        private const float line = 300;
+        private const float friction = 0.01f;
+        private const float _V = 2f;
+        private const float size = 30;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int y_pos = pictureBox1.Location.Y;
+            float r_pos = 500 - y_pos + v_pos;
+
+            if (r_pos < 0 && speed < 0)
+            {
+                speed *= -1;
+            }
+
+
+
+            float d = line - r_pos;
+            float force;
+
+            if (d > size / 2)
+            {
+                force = _V + gravity;
+            }
+            else if (d < size / 2)
+            {
+                force = gravity;
+            }
+            else
+            {
+                force = _V * (d + size / 2) + gravity;
+            }
+
+            force -= speed * friction;
+
+            speed += force * t;
+            r_pos = r_pos + speed * t;
+
+
+            listBox1.Items.Add("speed:" + (speed).ToString() +
+                ",force:" + (force).ToString() +
+                ",pos:" + (r_pos).ToString());
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+
+            int i_pos = (int)r_pos;
+            v_pos = r_pos - i_pos;
+            pictureBox1.Location = new Point(x_pos, 500 - i_pos);
+        }
+
+        private void timeStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timeReStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
         }
     }
 }
