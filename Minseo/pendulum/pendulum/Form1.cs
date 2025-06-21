2531916c7d948dd4ebd0d79d1693759d4dc44e8f
minjoowon <148769629+minjoowon@users.noreply.github.com>
2025-06-21 20:55:48 +0900

    asd

--- Minseo/pendulum/pendulum/Form1.cs
+++ Minseo/pendulum/pendulum/Form1.cs
@@ -17,83 +17,98 @@ namespace pendulum
         {
             InitializeComponent();
 
-
-
-
-
-            x_pos = pictureBox1.Location.X;
             pictureBox2.SendToBack();
             timer1.Start();
         }
 
-        public Bitmap ChangeOpacity(Image img, float opacityvalue)
-        {
-            Bitmap bmp = new Bitmap(img.Width, img.Height);
-            Graphics graphics = Graphics.FromImage(bmp);
-            ColorMatrix colormatrix = new ColorMatrix();
-            colormatrix.Matrix33 = opacityvalue;
-            ImageAttributes imgAttribute = new ImageAttributes();
-            imgAttribute.SetColorMatrix(colormatrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
-            graphics.DrawImage(img, new Rectangle(0, 0, bmp.Width, bmp.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, imgAttribute);
-            graphics.Dispose();
-            return bmp;
-        }
 
-
-        private static float num = 0;
+        private float force = 0;
         private static float speed = 0;
         private const float gravity = -0.5f;
-        private static float v_pos = 0;
-        private static int x_pos;
-        private static float t = 0.3f;
+        private static float position = 0;
+
+        private static float delta_time = 0.3f;
 
         private const float line = 300;
-        private const float friction = 0.01f;
+        private const float friction_공기 = 0.01f;
+        private const float friction_물 = 0.1f;
         private const float _V = 2f;
         private const float size = 30;
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            int y_pos = pictureBox1.Location.Y;
-            float r_pos = 500 - y_pos + v_pos;
 
-            if (r_pos < 0 && speed < 0)
+            if (position < 0 && speed < 0)
             {
                 speed *= -1;
             }
 
+            force = 0;
+
+            force += gravity;
+            force += Force_buoyancy();
+            force += speed * Friction();
 
 
-            float d = line - r_pos;
-            float force;
+            speed += force * delta_time;
+            position = position + speed * delta_time;
+
+            Print();
+
+            pictureBox1.Location = new Point(pictureBox1.Location.X, 500 - (int)position);
+        }
+
+        private float Force_buoyancy()
+        {
+            float d = line - position;
+            float force=0;
 
             if (d > size / 2)
             {
-                force = _V + gravity;
+                force = _V;
             }
             else if (d < size / 2)
             {
-                force = gravity;
+                
             }
             else
             {
-                force = _V * (d + size / 2) + gravity;
+                force = _V * (d + size / 2) ;
             }
 
-            force -= speed * friction;
+            return force;
+        }
 
-            speed += force * t;
-            r_pos = r_pos + speed * t;
+        private float Friction()
+        {
+            float friction;
+            if (position + size / 2 < line)
+            {
+                friction = friction_물;
+            }
+            else if (position - size / 2 > line)
+            {
+                friction = friction_공기;
+            }
+            else if (speed > 0)
+            {
+                friction = friction_공기;
+            }
+            else
+            {
+                friction = friction_물;
+            }
 
+            return -friction;
+        }
+
+        private void Print()
+        {
 
             listBox1.Items.Add("speed:" + (speed).ToString() +
                 ",force:" + (force).ToString() +
-                ",pos:" + (r_pos).ToString());
+                ",pos:" + (position).ToString());
             listBox1.SelectedIndex = listBox1.Items.Count - 1;
-
-            int i_pos = (int)r_pos;
-            v_pos = r_pos - i_pos;
-            pictureBox1.Location = new Point(x_pos, 500 - i_pos);
         }
 
         private void timeStop_Click(object sender, EventArgs e)
